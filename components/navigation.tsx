47c8d0e567f73894e7e92cef161d157091b46baa
Viktor <viktor.rumenov@grind.bg>
2026-05-15 17:40:36 +0300

    Fix: alignement of sign in and sign up buttons

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -25,11 +25,15 @@ const Navigation = () => {
           </Show>
         </div>
 
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
           <Show when="signed-out">
-            <SignInButton mode="modal" />
+            <SignInButton mode="modal">
+              <button className="text-md font-medium cursor-pointer">
+                Sign In
+              </button>
+            </SignInButton>
             <SignUpButton mode="modal">
-              <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              <button className="bg-foreground text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                 Sign Up
               </button>
             </SignUpButton>
